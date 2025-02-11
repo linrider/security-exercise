d9804f1b360299941d37d0dfe1320307f16d2746
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2025-02-11 14:33:07 +0200

    fix no args constructor error

--- src/main/java/ua/learning/security_exercise/model/Customer.java
+++ src/main/java/ua/learning/security_exercise/model/Customer.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     private int id;
 
